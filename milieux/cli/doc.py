d8fc86fc5c60150a43420003ef0bdd36763d6464
Jeremy Silver <jeremys@nessiness.com>
2024-11-01 14:10:13 -0400

    refactor: patch pdoc to not handle __all__ specially

--- milieux/cli/doc.py
+++ milieux/cli/doc.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass, field
 from pathlib import Path
+from pkgutil import ModuleInfo
 import time
-from typing import Literal, Union
+from types import ModuleType
+from typing import Any, Callable, Literal, Union
 
 from fancy_dataclass import ArgparseDataclass, CLIDataclass
+import pdoc.doc
+import pdoc.extract
 import pdoc.render
 import pdoc.web
 
@@ -12,6 +16,23 @@ from milieux.distro import get_packages
 
 
 DocFormat = Literal['markdown', 'google', 'numpy', 'restructuredtext']
+
+
+def _patch_pdoc() -> None:
+    """Override the default `__all__` behavior of pdoc so that it presents all submodules of a package even when it declares an `__all__` member."""
+    _iter_modules2: Callable[[ModuleType], dict[str, ModuleInfo]] = pdoc.extract.iter_modules2
+    _doc_init = pdoc.doc.Doc.__init__
+    def iter_modules2(module: ModuleType) -> dict[str, ModuleInfo]:
+        if hasattr(module, '__all__'):
+            del module.__all__
+        module.__all__ = None  # type: ignore[attr-defined]
+        return _iter_modules2(module)
+    def doc_init(self: Any, *args: Any, **kwargs: Any) -> None:
+        _doc_init(self, *args, **kwargs)
+        if hasattr(self.obj, '__all__'):
+            del self.obj.__all__
+    pdoc.extract.iter_modules2 = iter_modules2
+    pdoc.doc.Doc.__init__ = doc_init
 
 
 @dataclass
@@ -72,6 +93,7 @@ class DocBuild(CLIDataclass, command_name='build'):
         self.render_args.configure()
         start = time.perf_counter()
         logger.info(f'Building documentation to {self.output_dir}...')
+        _patch_pdoc()
         pdoc.pdoc(*self.pkg_args.all_packages, output_directory=self.output_dir)
         elapsed = time.perf_counter() - start
         logger.info(f'Build docs in {elapsed:.3g} sec')
@@ -104,6 +126,7 @@ class DocServe(CLIDataclass, command_name='serve'):
     def run(self) -> None:
         self.render_args.configure()
         logger.info('Serving documentation...')
+        _patch_pdoc()
         try:
             httpd = pdoc.web.DocServer((self.host, self.port), self.pkg_args.all_packages)
         except OSError as e:
